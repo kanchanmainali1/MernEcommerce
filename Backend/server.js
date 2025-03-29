b0d68a29022f4325e0d6921d99f68a38105c92a8
kanchanmainali1 <kanchanmainali1@gmail.com>
2025-03-29 23:22:19 +0545

    UserAccountBackend

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const authRoutes=require('./routes/auth/auth.route');
 const adminProductRoutes=require('./routes/admin/product.route');
 const userProductRoutes=require('./routes/users/product.route');
 const userCartRoutes=require('./routes/users/cart.route');
+const userAddressRoutes=require('./routes/users/address.route');
 const cors=require('cors');
 mongoose.connect(
 'mongodb+srv://mainalikanchan08:mainalikanchan2060@cluster0.o7whh.mongodb.net/'
@@ -32,6 +33,7 @@ app.use('/api/auth',authRoutes)
 app.use('/api/admin/products',adminProductRoutes) 
 app.use('/api/user/products',userProductRoutes)
 app.use('/api/user/cart',userCartRoutes)
+app.use( '/api/user/address',userAddressRoutes)
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
